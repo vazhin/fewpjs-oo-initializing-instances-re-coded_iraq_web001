757fab39a0b7f2079bddd06335015016369bed3a
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-12 21:13:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ class Lunch {
     this.drink = drink;
   }
 }
+
+class Dinner {
+  constructor(salad,soup,entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.dessert = dessert;
+  }
+}
